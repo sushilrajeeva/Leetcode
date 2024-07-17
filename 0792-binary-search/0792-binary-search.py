5ee4145036074ec8a55ec551a387cabaef4623b6
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-07-17 07:29:10 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        n = len(nums)
+        left, right = 0, n-1
+
+        while left<=right:
+            mid = int(left - (left - right)/2)
+
+            if nums[mid] == target:
+                return mid
+            
+            if nums[left] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+
+        
