10ad8e7787ab09ec9081cb4f13f4d10ce2bd12ef
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-07-27 18:54:01 -0400

    Time: 73 ms (91.41%), Space: 20.7 MB (22.03%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -14,22 +14,27 @@ class Codec:
         :rtype: str
         """
 
-        # Preorder
+        # i will perform a preorder traversal and as i go i store the node in a list 
+        # if i encounter None then i will store N in its place
+        # at the end i will return the string by coverting the list to string by using join and using ',' as a delimiter
+
         string: List[str] = []
-        def toString(root: Optional[TreeNode]) -> None:
+
+        def preorder(root: Optional[TreeNode]) -> None:
+
             if not root:
                 string.append("N")
                 return
 
             string.append(str(root.val))
-            toString(root.left)
-            toString(root.right)
+            preorder(root.left)
+            preorder(root.right)
             return
 
-        toString(root)
+        preorder(root)
 
         return ",".join(string)
-            
+        
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -38,25 +43,31 @@ class Codec:
         :rtype: TreeNode
         """
 
-        arr = data.split(",")
+        # I know that the seralization is in inorder and where ever N is present that means it is end of the node
+        # i will convert string to list by splitting it by ','
+        arr: List[str] = data.split(",")
+        # will create a global index counter to keep track of which node i am currently in
         self.i = 0
+        
+        # Recursively construct the Binary Tree and return the root
+        def dfs() -> Optional[TreeNode]:
 
-        def dfs():
+            # if i reach N then i must return None
             if arr[self.i] == "N":
                 self.i += 1
                 return None
-
-            node: Optional[TreeNode] = TreeNode(int(arr[self.i]))
-            self.i += 1
             
+            # if arr[self.i] != 'N' then i must create a node of value arr[self.i]
+            node: Optional[TreeNode] = TreeNode(arr[self.i])
+            self.i += 1
+
+            # recursively use this function to build the left and right subtrees -> self.i is global so it takes care of counter or index position of the arr
             node.left = dfs()
             node.right = dfs()
 
             return node
-        
+
         return dfs()
-
-
         
 
 # Your Codec object will be instantiated and called as such:
