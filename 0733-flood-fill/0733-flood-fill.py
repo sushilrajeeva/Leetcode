2ee770c4b9a168b0d41ebc75145eaaa50ccf3cc5
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-08-05 01:51:34 -0400

    Time: 62 ms (83.59%), Space: 16.6 MB (63.35%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -5,19 +5,19 @@ class Solution:
         visited = [[False] * m for _ in range(n)]
         key = image[sr][sc]
 
-        def dfs(sr: int, sc: int):
+        def dfs(sr: int, sc: int, image: List[List[int]], visited: List[List[int]], key: int):
             if sr < 0 or sr >= n or sc < 0 or sc >= m or image[sr][sc] != key or visited[sr][sc]:
                 return
             
             visited[sr][sc] = True
             image[sr][sc] = color
 
-            dfs(sr - 1, sc) # UP
-            dfs(sr + 1, sc) # DOWN
-            dfs(sr, sc - 1) # LEFT
-            dfs(sr, sc + 1) # RIGHT
+            dfs(sr - 1, sc, image, visited, key) # UP
+            dfs(sr + 1, sc, image, visited, key) # DOWN
+            dfs(sr, sc - 1, image, visited, key) # LEFT
+            dfs(sr, sc + 1, image, visited, key) # RIGHT
 
         
-        dfs(sr, sc)
+        dfs(sr, sc, image, visited, key)
 
         return image
