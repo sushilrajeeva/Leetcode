a05ffe09963cc0203c78e672d36ff48add43480a
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-08-05 04:37:24 -0400

    Time: 47 ms (93.17%), Space: 17 MB (73.34%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,35 +1,27 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # Helper function to find the root of a node
+        def find(node):
+            if parent[node] != node:
+                parent[node] = find(parent[node])  # Path compression
+            return parent[node]
 
+        # Helper function to union two nodes
+        def union(node1, node2):
+            root1 = find(node1)
+            root2 = find(node2)
+            if root1 != root2:
+                parent[root2] = root1
+                return False
+            return True  # Cycle detected
+
+        # Initialize parent array for each node
         n = len(edges)
+        parent = [i for i in range(n + 1)]  # Using 1-based index
 
-        adj = [[] for _ in range(n+1)]
-
-
-    
-        def dfs(node, parent):
-            visited[node] = True
-
-            for neighbor in adj[node]:
-                if not visited[neighbor]:
-                    visited[neighbor] = True
-                    if dfs(neighbor, node):
-                        return True
-                elif visited[neighbor] and neighbor != parent: return True
-
-            return False
-
-        for edge in edges:
-            visited = [False] * (n+1)
-
-            e1, e2 = edge[0], edge[1]
-
-            adj[e1].append(e2)
-            adj[e2].append(e1)
-
-            if dfs(e1, -1):
-                return [e1, e2]
+        # Process each edge
+        for node1, node2 in edges:
+            if union(node1, node2):
+                return [node1, node2]
 
         return []
-                        
-        
