b87782aa145fddfa837a59c8e649d74e25325ce5
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-08-16 05:46:04 -0400

    Time: 111 ms (91.96%), Space: 18 MB (53.21%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -9,38 +9,42 @@ class Solution:
 
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        visited = [[False] * m for _ in range(n)]
+        def isValid(row: int, col: int) -> bool:
+            return 0 <= row < n and 0 <= col < m
 
         def dfs(row: int, col: int):
-            if board[row][col] == "O" and not visited[row][col]:
-                visited[row][col] = True
-                for r, c in directions:
-                    newRow, newCol = row + r, col + c
-                    if (0 <= newRow < n) and (0 <= newCol < m) and not visited[newRow][newCol] and board[newRow][newCol] == "O":
-                        dfs(newRow, newCol)
-            else: return
+            if not isValid(row, col) or board[row][col] != "O":
+                return
+
+            board[row][col] = "-O"
+
+            for direction in directions:
+                newRow, newCol = direction[0] + row, direction[1] + col
+                dfs(newRow, newCol)
         
         # travel top right and bottom right boundry
         for c in range(m):
-            if board[0][c] == "O" and not visited[0][c]:
+            if board[0][c] == "O":
                 dfs(0, c)
 
-            if board[n-1][c] == "O" and not visited[n-1][c]:
+            if board[n-1][c] == "O":
                 dfs(n-1, c)
 
         
         # travel left down and right down boundry
         for r in range(n):
-            if board[r][0] == "O" and not visited[r][0]:
+            if board[r][0] == "O":
                 dfs(r, 0)
 
-            if board[r][m-1] == "O" and not visited[r][m-1]:
+            if board[r][m-1] == "O":
                 dfs(r, m-1)
 
         for i in range(n):
             for j in range(m):
-                if board[i][j] == "O" and not visited[i][j]:
+                if board[i][j] == "O":
                     board[i][j] = "X"
+                elif board[i][j] == "-O":
+                    board[i][j] = "O"
 
         return board
 
