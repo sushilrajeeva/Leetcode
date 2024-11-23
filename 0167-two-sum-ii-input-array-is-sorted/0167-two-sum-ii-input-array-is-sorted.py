15031e8da6e152c3523a11bada49c7e49eeada4e
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-11-23 05:10:58 -0500

    Time: 4 ms (51.95%), Space: 17.7 MB (60.78%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,19 +1,28 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
-        n: int = len(numbers)
 
-        left: int = 0
-        right: int = n-1
+        # There is a guaranteed solution
+        # there are always valid inputs
+
+        # Edge case 1: I find my solution in first two , last two or first and last position , avoiding unnecessary computaiton 
+
+        pairs = [[0, 1], [0, -1], [-1, -2]]
+        n = len(numbers)
+
+        for pair in pairs:
+            if numbers[pair[0]] + numbers[pair[1]] == target:
+                first = pair[0] + 1 if pair[0] >= 0 else len(numbers) + pair[0] + 1
+                second = pair[1] + 1 if pair[1] >= 1 else len(numbers) + pair[1] + 1
+                return [min(first, second), max(first, second)]
+
+        left = 0
+        right = n - 1
 
         while left < right:
-            cal: int = numbers[left] + numbers[right]
-            if cal == target: return [left+1, right+1]
-            if cal < target:
-                left +=1
-            else: right -= 1
+            if numbers[left] + numbers[right] == target: return [left+1, right+1]
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            else: left +=1
 
-        return [-1, -1]
-
-            
-
+        return [left + 1, right + 1]
+        
