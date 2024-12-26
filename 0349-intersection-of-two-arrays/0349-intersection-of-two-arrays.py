b51f848567b6e993078cfb539f80f8c25b4f00aa
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-12-25 23:57:50 -0500

    Time: 0 ms (100%), Space: 17.7 MB (11.88%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,27 +1,18 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-
-        return list(set(nums1).intersection(set(nums2)))
         
-        # n, m = len(nums1), len(nums2)
-        # result = []
+        n, m = len(nums1), len(nums2)
 
-        # index1, index2 = 0, 0
+        buffer: List[int] = [0] * 1001
+        res: List[int] = []
+
+        for i in range(n):
+            if buffer[nums1[i]] == 0:
+                buffer[nums1[i]] += 1
         
-        # freq1 = [0] * (1001)
-        # freq2 = [0] * (1001)
+        for j in range(m):
+            if buffer[nums2[j]] == 1:
+                buffer[nums2[j]] = 0
+                res.append(nums2[j])
 
-        # for i in range(n):
-        #     freq1[nums1[i]] += 1
-        
-        # for j in range(m):
-        #     freq2[nums2[j]] += 1
-
-        # for i in range(len(freq1)):
-        #     if freq1[i] > 0 and freq2[i] > 0:
-        #         result.append(i)
-
-        # return result
-
-        
-        
+        return res
