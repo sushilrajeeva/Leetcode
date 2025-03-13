1d6d6f3df3fb187f44662457c6946e303c9895df
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-03-13 13:35:36 -0400

    Time: 911 ms (49.67%), Space: 62.9 MB (99.1%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
@@ -1,40 +1,40 @@
 from typing import List
 
 class Solution:
+    def canTransform(self, nums: List[int], queries: List[List[int]], k: int) -> bool:
+        n = len(nums)
+        total = 0
+        diff = [0] * (n + 1)
 
-    def canTransform(self, nums: List[int], queries: List[int], right: int) -> bool:
-        n: int = len(nums)
-        total: int = 0
-
-        diff: List[int] = [0] * (n+1)
-
-        for i in range(right):
-            l: int = queries[i][0]
-            r: int = queries[i][1]
-            val: int = queries[i][2]
-
+        # Process the first k queries to build the difference array
+        for i in range(k):
+            l, r, val = queries[i]
             diff[l] += val
-            diff[r+1] -= val
+            if r + 1 < len(diff):
+                diff[r + 1] -= val
 
+        # Check if each element can be reduced to zero
         for i in range(n):
             total += diff[i]
-            if total < nums[i]: return False
+            if total < nums[i]:
+                return False
         return True
 
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        m = len(queries)
+        left = 0
+        right = m
 
-        n: int = len(nums)
-        m: int = len(queries)
-        left: int = 0
-        right: int = m
+        # If even all queries can't transform the array, return -1
+        if not self.canTransform(nums, queries, right):
+            return -1
 
-        if not self.canTransform(nums, queries, right): return -1
-
+        # Binary search for the minimum number of queries needed
         while left <= right:
-            mid: int = int(left + (right - left)//2)
+            mid = (left + right) // 2
             if self.canTransform(nums, queries, mid):
                 right = mid - 1
             else:
                 left = mid + 1
+
         return left
-       
