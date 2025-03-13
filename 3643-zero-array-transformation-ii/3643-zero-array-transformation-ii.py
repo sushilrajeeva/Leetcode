3d36e1e2c4f69cad6916ad6cf6820946ea9e460f
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-03-13 13:33:26 -0400

    Time: 833 ms (52.81%), Space: 63 MB (86.29%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
@@ -1,39 +1,40 @@
 from typing import List
 
 class Solution:
+
+    def canTransform(self, nums: List[int], queries: List[int], right: int) -> bool:
+        n: int = len(nums)
+        total: int = 0
+
+        diff: List[int] = [0] * (n+1)
+
+        for i in range(right):
+            l: int = queries[i][0]
+            r: int = queries[i][1]
+            val: int = queries[i][2]
+
+            diff[l] += val
+            diff[r+1] -= val
+
+        for i in range(n):
+            total += diff[i]
+            if total < nums[i]: return False
+        return True
+
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        n = len(nums)
-        m = len(queries)
-        
-        # Helper function to check if the first k queries are sufficient.
-        # We use a difference array to quickly calculate the total available decrement
-        # for each index.
-        def can_zero(k: int) -> bool:
-            diff = [0] * (n + 1)
-            for i in range(k):
-                l, r, v = queries[i]
-                diff[l] += v
-                if r + 1 < n:
-                    diff[r + 1] -= v
-            
-            current = 0
-            for i in range(n):
-                current += diff[i]
-                # If the total available decrement at index i is less than the value of nums[i],
-                # then it's not possible to zero out this index.
-                if current < nums[i]:
-                    return False
-            return True
-        
-        # Binary search for the minimum k that can zero out the array.
-        lo, hi = 0, m + 1  # hi is set to m+1 so that if no k in [0, m] works, we detect it.
-        res = -1
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if can_zero(mid):
-                res = mid
-                hi = mid  # try to find a smaller k
+
+        n: int = len(nums)
+        m: int = len(queries)
+        left: int = 0
+        right: int = m
+
+        if not self.canTransform(nums, queries, right): return -1
+
+        while left <= right:
+            mid: int = int(left + (right - left)//2)
+            if self.canTransform(nums, queries, mid):
+                right = mid - 1
             else:
-                lo = mid + 1
-        
-        return res
+                left = mid + 1
+        return left
+       
