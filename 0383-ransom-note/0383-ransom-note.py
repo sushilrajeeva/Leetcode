0b7b75ad70337132cef5ac64a0bc30826ffa2616
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-04-17 01:31:40 -0400

    Time: 3 ms (97.98%), Space: 17.9 MB (82.7%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,24 +1,8 @@
 class Solution:
-
-    def getCharVal(self, charecter: str) -> int:
-        return ord(charecter) - 97
-
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-
-        if len(ransomNote) > len(magazine): return False
-        
-        temp = [0] * 26
-
-        for ele in magazine:
-            temp[self.getCharVal(ele)] += 1
-
-        for ele in ransomNote:
-            temp[self.getCharVal(ele)] -= 1
-            if temp[self.getCharVal(ele)] < 0:
+        if len(ransomNote)>len(magazine):
+            return False
+        for c in set(ransomNote):
+            if magazine.count(c)<ransomNote.count(c):
                 return False
         return True
-
-
-
-
-        
