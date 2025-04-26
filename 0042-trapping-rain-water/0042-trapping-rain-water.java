ebd35eb67bb1b3343ec53eadea9934a041736861
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-04-26 15:10:08 -0400

    Time: 1 ms (62.62%), Space: 46.1 MB (74.23%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,28 +1,33 @@
 class Solution {
     public int trap(int[] height) {
+
         int n = height.length;
-        int trappedWater = 0;
-        int [] leftMax = new int[n];
-        int [] rightMax = new int[n];
+        if (n == 0) return 0;
 
+        int [] prefixArr = new int[n];
+        int [] suffixArr = new int[n];
+
+        // Computing prefixArr and suffixArr
         int tempLeft = 0;
-        for (int i =0; i < n; i ++) {
+        int tempRight = 0;
+        for (int i = 0; i < n; i++) {
             tempLeft = Math.max(tempLeft, height[i]);
-            leftMax[i] = tempLeft;
+            tempRight = Math.max(tempRight, height[n-i-1]);
+            prefixArr[i] = tempLeft;
+            suffixArr[n-i-1] = tempRight;
         }
 
-        int tempRight = 0;
-        for (int i = n - 1; i >=0; i--) {
-            tempRight = Math.max(tempRight, height[i]);
-            rightMax[i] = tempRight;
-        }
+        int water = 0;
 
         for (int i = 0; i < n; i++) {
-            int computation = Math.min(leftMax[i], rightMax[i]) - height[i];
-            trappedWater += computation > 0 ? computation : 0;
+            int computation = Math.min(prefixArr[i], suffixArr[i]) - height[i];
+            water += computation > 0 ? computation : 0;
         }
 
-        return trappedWater;
+        return water;
+
         
     }
+
+
 }
