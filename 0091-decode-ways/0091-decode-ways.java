3f7b15c37a4a2bbb7ac8518d3490da5a894b2d2f
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-04-26 11:14:43 -0400

    Time: 0 ms (100%), Space: 41.8 MB (78.18%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -2,27 +2,31 @@ class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         if (n == 0) return 0;
-        int [] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = (s.charAt(0) == '0') ? 0 : 1;
-        if (dp[1] == 0) return 0; 
+        int prev_2 = 1;
+        int prev_1 = (s.charAt(0) == '0') ? 0 : 1;
+        if (prev_1 == 0) return 0; 
+
+
         
         for (int i = 2; i <= n; i++) {
             // Single-digit decode: consider character s[i-1]
             char cur = s.charAt(i - 1);
+            int computation = 0;
             if (cur != '0') {
-                dp[i] += dp[i - 1];
+                computation += prev_1;
             }
             
             // Two-digit decode: consider substring s[i-2..i-1]
             char prev = s.charAt(i - 2);
             int twoDigit = (prev - '0') * 10 + (cur - '0');
             if (twoDigit >= 10 && twoDigit <= 26) {
-                dp[i] += dp[i - 2];
+                computation += prev_2;
             }
+            prev_2 = prev_1;
+            prev_1 = computation;
         }
         
-        return dp[n];
+        return prev_1;
         
     }
 }
