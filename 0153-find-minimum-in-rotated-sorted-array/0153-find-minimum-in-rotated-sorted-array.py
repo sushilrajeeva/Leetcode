60140dd4e82011e7d7342e5ed66abbafdeb0f243
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-04-28 02:39:55 -0400

    Time: 0 ms (100%), Space: 18 MB (80.65%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,41 +1,28 @@
 class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        if n == 1: return nums[0]
 
-    def isRotated(self, nums: List[int]) -> bool:
-        return nums[0] > nums[-1]
+        # if array is not rotated
+        if nums[0] < nums[-1]:
+            return nums[0]
 
-    def getBreakIndex(self, nums: List[int]) -> int:
+        # if rotated find peak
+
         left = 0
-        right = len(nums)-1
+        right = n - 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
 
-        while left<=right:
-            mid = left + (right-left)//2
-            if mid > 0 and mid < len(nums)-1 and nums[mid-1] > nums[mid] and nums[mid] < nums[mid+1]:
-                return mid
+            if mid < n - 1 and nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            elif mid > 0 and nums[mid-1] > nums[mid]:
+                return nums[mid]
+                
             if nums[mid] > nums[-1]:
                 left = mid + 1
-            else:
-                right = mid - 1
+            else: right = mid - 1
 
-        return left
-
-
-
-    def findMin(self, nums: List[int]) -> int:
-
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        if n == 2:
-            return nums[0] if nums[0] < nums[1] else nums[1]
-
-        if not self.isRotated(nums):
-            return nums[0]
-
-        breakIndex = self.getBreakIndex(nums)
-
-        return nums[breakIndex]
-        
-
-        
-
-        
+        return -1
