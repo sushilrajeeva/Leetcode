f2bb879eebfc8168a8abf65d5d6fffbbfcb8af80
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-04-29 16:33:18 -0400

    Time: 65 ms (90.05%), Space: 29.6 MB (77.76%) - LeetHub

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,28 +1,14 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        if n == 0 or k < 1:
-            return 0
-        if n == 1:
-            # only one subarray [0..0]
-            return 1 if k == 1 else 0
+        max_element = max(nums)
+        ans = start = max_elements_in_window = 0
 
-        maxEle = max(nums)
-        max_pos = deque()  # indices where nums[i] == maxEle
-        res = 0
-
-        for right, v in enumerate(nums):
-            if v == maxEle:
-                max_pos.append(right)
-
-            # once we've seen ≥ k copies of maxEle, every subarray ending at `right`
-            # that starts at index 0..threshold will contain k of them
-            if len(max_pos) >= k:
-                threshold = max_pos[-k]
-                res += (threshold + 1)
-
-        return res
-
+        for end in range(len(nums)):
+            if nums[end] == max_element:
+                max_elements_in_window += 1
+            while max_elements_in_window == k:
+                if nums[start] == max_element:
+                    max_elements_in_window -= 1
+                start += 1
+            ans += start
+        return ans
