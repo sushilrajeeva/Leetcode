982b36901d8fcb028933672f75c3a6da75d3b201
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-07-03 17:15:32 -0400

    Time: 439 ms (30.35%), Space: 38.9 MB (49.91%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,22 @@
+from collections import *
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n: int = len(coins)
+        memo = [[-1] * (amount + 1) for _ in range(n)]
+
+        def numberOfWays(i: int, amount: int) -> int:
+            if amount == 0:
+                return 1
+            if i == n:
+                return 0
+            if memo[i][amount] != -1:
+                return memo[i][amount]
+            
+            if coins[i] > amount:
+                memo[i][amount] = numberOfWays(i + 1, amount)
+            else:
+                memo[i][amount] = numberOfWays(i, amount - coins[i]) + numberOfWays(i + 1, amount)
+
+            return memo[i][amount]
+
+        return numberOfWays(0, amount)
