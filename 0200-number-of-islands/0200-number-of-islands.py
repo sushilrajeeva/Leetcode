d3333b0dbeb8baa38ec0306d3b332b337e9d0091
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-07-06 06:03:33 -0400

    Time: 269 ms (31.8%), Space: 20.4 MB (38.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,37 @@
 class Solution:
+
+    def isValidMove(self, grid: List[List[int]], row: int, col: int, visited: List[List[int]]) -> bool:
+        return 0 <= row < len(grid) and 0 <= col < len(grid[0]) and not visited[row][col] and grid[row][col] == "1"
+
     def numIslands(self, grid: List[List[str]]) -> int:
+        count: int = 0
+        m: int = len(grid)
+        n: int = len(grid[0])
+        visited: List[List[int]] = [[False] * n for _ in range(m)]
+        directions: List[Tuple[int, int]] = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        n = len(grid)
-        m = len(grid[0])
+        def dfs(row: int, col: int) -> None:
 
-        visited = [[False]* m for _ in range(n)]
+            if not self.isValidMove(grid, row, col, visited):
+                return
 
-        count = 0
+            visited[row][col] = True
 
-        def dfs(i: int, j: int) -> None:
-            if i < 0 or i >= n or j < 0 or j >= m or visited[i][j] or grid[i][j] == "0":
-                return 
-            visited[i][j] = True
-            
-            dfs(i-1, j) # up
-            dfs(i+1, j) # down
-            dfs(i, j-1) # left
-            dfs(i, j+1) # right
-        
-        for i in range(n):
-            for j in range(m):
-                if not visited[i][j] and grid[i][j] == "1":
+            for direction in directions:
+                new_row: int = row + direction[0]
+                new_col: int = col + direction[1]
+                dfs(new_row, new_col)
+
+        for r in range(m):
+            for c in range(n):
+                if not visited[r][c] and grid[r][c] == "1":
                     count += 1
-                    dfs(i, j)
+                    dfs(r, c)
+                    
 
         return count
+
+        
+
+
         
