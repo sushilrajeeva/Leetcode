3265502b96d77e0c6057c64b03c3140fecdeb095
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-07-07 04:31:31 -0400

    Time: 4518 ms (21%), Space: 18.5 MB (36.38%) - LeetHub

--- 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
@@ -6,6 +6,27 @@ class Solution:
     def isValidMove(self, grid: List[List[int]], visited: List[List[int]], row: int, col: int) -> bool:
         return 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] == 0 and not visited[row][col]
 
+    def bfs(self, grid: List[List[int]], dist_sum: List[List[int]], reach_count: List[List[int]], row: int, col: int) -> None:
+        queue = deque([(row, col, 0)])
+        m: int = len(grid)
+        n: int = len(grid[0])
+        directions: List[Tuple[int, int]] = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        visited = [[False] * n for _ in range(m)]
+        visited[row][col] = True
+        while queue:
+            r, c, d = queue.popleft()
+            for dr, dc in directions:
+                new_row = r + dr
+                new_col = c + dc
+
+                if self.isValidMove(grid, visited, new_row, new_col):
+                    visited[new_row][new_col] = True
+                    dist_sum[new_row][new_col] += d + 1
+                    reach_count[new_row][new_col] += 1
+
+                    queue.append((new_row, new_col, d + 1))
+
+
     def shortestDistance(self, grid: List[List[int]]) -> int:
 
         m: int = len(grid)
@@ -22,21 +43,8 @@ class Solution:
     
                 if grid[row][col] == 1:
                     total_buildings += 1
-                    queue = deque([(row, col, 0)])
-                    visited = [[False] * n for _ in range(m)]
-                    visited[row][col] = True
-                    while queue:
-                        r, c, d = queue.popleft()
-                        for dr, dc in directions:
-                            new_row = r + dr
-                            new_col = c + dc
-
-                            if self.isValidMove(grid, visited, new_row, new_col):
-                                visited[new_row][new_col] = True
-                                dist_sum[new_row][new_col] += d + 1
-                                reach_count[new_row][new_col] += 1
-
-                                queue.append((new_row, new_col, d + 1))
+                    self.bfs(grid, dist_sum, reach_count, row, col)
+                    
 
         best = float("inf")
         for r in range(m):
