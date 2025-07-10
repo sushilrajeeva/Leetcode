c0702c937ac9e6aec00c6331ce4cf5f3ae2d464b
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-07-10 03:33:28 -0400

    Time: 0 ms (100%), Space: 18.4 MB (86.9%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -4,71 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
+from typing import *
 from collections import deque
-
-class Queue:
-
-    def __init__(self):
-        self.queue = deque()
-
-    def isEmpty(self) -> bool:
-        return len(self.queue) == 0
-
-    def peak(self) -> int:
-        if self.isEmpty():
-            raise IndexError("Can't peak at empty Queue")
-        return self.queue[0]
-
-    def enqueue(self, value: int) -> None:
-        self.queue.append(value)
-
-    def dequeue(self) -> int:
-        if self.isEmpty():
-            raise IndexError("Can't dequeue an empty Queue")
-        return self.queue.popleft()
-
-
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        bfs: List[List[int]] = []
+        if not root: return bfs
+        queue = deque([root])
 
-        if not root:
-            return []
+        while queue:
+            size: int = len(queue)
+            level: List[int] = [0] * size
+            for i in range(size):
+                node: Optional[TreeNode] = queue.popleft()
+                level[i] = node.val
 
-        result = []
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            bfs.append(level)
 
-        queue = Queue()
-        queue.enqueue(root)
-        queue.enqueue(None) # Marker to mark end of the level
+        return bfs
 
-        while not queue.isEmpty():
-
-            currentNode = queue.dequeue() # pop the top of queue element
-
-            if currentNode is None: # if we hit a marker check if it is empty queue
-
-                if not queue.isEmpty():
-                    result.append([])
-                    queue.enqueue(None) # Add a marker at the end
-            
-            else:
-
-                # if current Node is not empty then check if it is the root node
-                if not result or currentNode == root:
-                    result.append([currentNode.val])
-                else:
-                    result[-1].append(currentNode.val)
-
-                # Check if there are left and right child, if so then enqueue them to the queue
-                if currentNode.left:
-                    queue.enqueue(currentNode.left)
-                if currentNode.right:
-                    queue.enqueue(currentNode.right)
-
-        if not result[-1]:
-            result.pop()
-
-        return result
 
 
         
