e76a86b02b6cae03e652ea29fc46482961d2cb33
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-07-10 03:08:07 -0400

    Time: 0 ms (100%), Space: 18 MB (48.76%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -1,65 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 from collections import deque
-from typing import List, Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Queue:
-    def __init__(self):
-        self.queue = deque()
-
-    def isEmpty(self) -> bool:
-        return len(self.queue) == 0
-
-    def peek(self) -> Optional[TreeNode]:
-        if not self.isEmpty():
-            return self.queue[0]
-        raise IndexError("Queue is Empty")
-    
-    def enqueue(self, value: TreeNode) -> None:
-        self.queue.append(value)
-
-    def dequeue(self) -> Optional[TreeNode]:
-        if not self.isEmpty():
-            return self.queue.popleft()
-        raise IndexError("Queue is Empty")
-
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
+        flag: bool = True
+        queue = deque([root])
+        res: List[List[int]] = []
+        if not root: return res
 
-        result = []
-        queue = Queue()
-        queue.enqueue(root)
-        queue.enqueue(None)
-        current_level = []
-        left_to_right = True
+        while queue:
+            level_len = len(queue)
+            level = deque()
+            for i in range(level_len):
+                node: Optional[TreeNode] = queue.popleft()
+                if flag:
+                    level.append(node.val)
+                else:
+                    level.appendleft(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
 
-        while not queue.isEmpty():
-            currentNode = queue.dequeue()
+            flag  = False if flag else True
+            res.append(list(level))
+        
+        return res
 
-            if currentNode is None:
-                if not queue.isEmpty():
-                    if not left_to_right:
-                        current_level.reverse()
-                    result.append(current_level)
-                    current_level = []
-                    left_to_right = not left_to_right
-                    queue.enqueue(None)
-            else:
-                current_level.append(currentNode.val)
-                if currentNode.left:
-                    queue.enqueue(currentNode.left)
-                if currentNode.right:
-                    queue.enqueue(currentNode.right)
 
-        if current_level:
-            if not left_to_right:
-                current_level.reverse()
-            result.append(current_level)
 
-        return result
+
+
+        
