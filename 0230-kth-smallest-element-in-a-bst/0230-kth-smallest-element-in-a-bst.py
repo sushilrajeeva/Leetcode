807dd8b967044978db227133080d1cabc4db3e09
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-07-10 04:53:18 -0400

    Time: 0 ms (100%), Space: 20.9 MB (93.31%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -12,12 +12,15 @@ class Solution:
         def inorder(node: Optional[TreeNode]):
             if not node or self.result is not None:
                 return
+
             inorder(node.left)
             self.count += 1
+            
             if self.count == k:
                 self.result = node.val
-                return
             inorder(node.right)
 
         inorder(root)
+
         return self.result
+        
