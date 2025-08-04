f438aa6f18c77c0f12a2e6302ba4c22f6b879105
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-08-04 04:30:14 -0400

    Time: 4944 ms (27.99%), Space: 17.7 MB (87.07%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,38 +1,45 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
 
-        R: int = len(board)
-        C: int = len(board[0])
+        self.ROWS: int = len(board)
+        self.COLS: int = len(board[0])
+        self.board = board
+        self.directions: List[Tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        self.word = word
 
-        visited: List[List[bool]] = [[False] * C for _ in range(R)]
-        directions: Tuple[Tuple[int, int]] = ((0, 1), (1, 0), (0, -1), (-1, 0))
-
-        def isValid(r: int, c: int) -> bool:
-            return 0 <= r < R and 0 <= c < C
-
-        def dfs(r: int, c: int, i) -> bool:
-            # 1) Out of bounds or wrong letter or already used?
-            if not isValid(r, c) or visited[r][c] or board[r][c] != word[i]:
-                return False
-
-            # 2) If it is the last letter of the word, we are done
-            if i == len(word) - 1:
-                return True
-            
-            # exploring all the directions
-            visited[r][c] = True
-            for dr, dc in directions:
-                if dfs(r + dr, c + dc, i + 1):
-                    return True
-
-            # backtracking
-            visited[r][c] = False
-            return False
-
-        # Try each cell
-        for r in range(R):
-            for c in range(C):
-                if dfs(r, c, 0):
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.backtrack(row, col, 0):
                     return True
         return False
+
+    def backtrack(self, row: int, col: int, i: int) -> bool:
+
+        if i == len(self.word):
+            return True
+
+        if (
+            not self.isValid(row, col)
+            or self.board[row][col] != self.word[i]
+        ): return False
+
+        original: str = self.board[row][col]
+        self.board[row][col] = "#"
+
+        # exploring all 4 neighboring directions
+        for r, c in self.directions:
+            # sudden-death return, no cleanup.
+            if self.backtrack(row + r, col + c, i + 1):
+                return True
+
+        # reverting the marking
+        self.board[row][col] = original
+
+        # Tried all directions, and did not find the match
+        return False
+
+
+    def isValid(self, r: int, c: int) -> bool:
+        return 0 <= r < self.ROWS and 0 <= c < self.COLS
+
         
