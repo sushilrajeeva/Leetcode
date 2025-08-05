5c6893f825f5a377cd43820239102c03b9ea98bd
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-08-05 04:24:00 -0400

    Time: 24 ms (47.11%), Space: 18.3 MB (51.52%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,31 +1,38 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
-        n: int = len(grid)
-        m: int = len(grid[0])
 
-        def isValid(row: int, col: int) -> bool:
-            if row < 0 or row >= n or col < 0 or col >= m: return False
-            return True
+        rows: int = len(grid)
+        cols: int = len(grid[0])
 
-        def dfs(row: int, col: int) -> int:
-            if not isValid(row, col) or grid[row][col] == 0:
+        directions: List[Tuple[int, int]] = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+        def isValid(row: int, col: int) -> None:
+            return 0 <= row < rows and 0 <= col < cols
+
+        def dfs(row: int, col: int) -> bool:
+            if not isValid(row, col) or grid[row][col] != 1:
                 return 0
 
-            grid[row][col] = 0
+            grid[row][col] = -1
+            
             area = 1
-            area += dfs(row+1, col)
-            area += dfs(row-1, col)
-            area += dfs(row, col+1)
-            area += dfs(row, col-1)
 
+            for dr, dc in directions:
+                area += dfs(row + dr, col + dc)
+            
             return area
+        
+        max_area: int = 0
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    area = dfs(row, col)
+                    max_area = max(max_area, area)
+                
+        return max_area
 
-        maxArea: int = 0
 
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] != 0:
-                    maxArea = max(maxArea, dfs(i, j))
 
-        return maxArea
+
+
+        
