4791b263f82fa59ae190e479eca01d6855861628
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-08-06 01:40:51 -0400

    Time: 0 ms (100%), Space: 42 MB (68.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,46 +1,44 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // checking if array is shifted or not
+
         int n = nums.length;
-        if (n == 0) return -1;
-        if (n == 1) {
-            return nums[0] == target ? 0 : -1;
+
+        if (n == 0){
+            return -1;
+        }
+        if (nums[0] == target) {
+            return 0;
+        }
+        if (nums[n - 1] == target) {
+            return n - 1;
         }
 
-        if (isRotated(nums)){
-            int breakIndex = getBreakIndex(nums);
-            if (nums[breakIndex] == target) { 
-                return breakIndex;
+        if (isRotated(nums)) {
+            // do something
+            int breakingIndex = getBreakingIndex(nums);
+            int firstPart = binarySearch(nums, 0, breakingIndex - 1, target);
+            if ((firstPart != -1) && (nums[firstPart] == target)) {
+                return firstPart;
             }
-            int firstPart = binarySearch(nums, 0, breakIndex, target);
-            if (firstPart == -1) {
-                return binarySearch(nums, breakIndex, n-1, target);
-            }
-            return firstPart;
+            return binarySearch(nums, breakingIndex, n - 1, target);
         }
-
         return binarySearch(nums, 0, n-1, target);
-
-
+        
     }
 
-    public boolean isRotated(int[] nums) {
-        return nums[0] > nums[nums.length-1];
-    }
-
-    public int getBreakIndex(int[] nums) {
-        int left = 0;
+    private int getBreakingIndex(int[] nums) {
         int n = nums.length;
+        int left = 0;
         int right = n - 1;
 
         while (left <= right) {
-            int mid = (int)(left + (right - left) / 2);
+            int mid = (int)(left + (right - left)/2);
 
-            if (mid > 0 && mid < n - 1 && nums[mid - 1] > nums[mid] && nums[mid] < nums[mid + 1]) {
+            if (isValidIndex(mid, n) && isBreakingPoint(nums, mid)) {
                 return mid;
             }
 
-            if (nums[mid] > nums[n-1]) {
+            if (nums[mid] >= nums[n - 1]) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
@@ -50,22 +48,31 @@ class Solution {
         return left;
     }
 
-    public int binarySearch(int[] nums, int left, int right, int target) {
-        
+    private boolean isValidIndex(int mid, int n) {
+        return (mid > 0 && mid < n - 1);
+    }
+
+    private boolean isBreakingPoint(int[] nums, int mid) {
+        return (nums[mid] < nums[mid - 1] && nums[mid] < nums[mid + 1]);
+    }
+
+    private boolean isRotated(int[] nums) {
+        return nums[0] > nums[nums.length - 1];
+    }
+
+    private int binarySearch(int[] nums, int left, int right, int target) {
         while (left <= right) {
-            int mid = (int) (left + (right - left) / 2);
+            int mid = (int)(left + (right - left)/2);
 
             if (nums[mid] == target) {
                 return mid;
             }
-
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
+            if (nums[mid] < target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-
         return -1;
     }
 }
