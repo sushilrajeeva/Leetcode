32e5cf677b197c30a039bf1506d06d7947292253
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-08-07 08:40:49 -0400

    Time: 1 ms (98.3%), Space: 41 MB (71.43%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        while(maxHeap.size() > 1) {
+            int stone1 = maxHeap.poll();
+            int stone2 = maxHeap.poll();
+
+            int newStone = stone1 - stone2;
+            
+            if(newStone > 0) {
+                maxHeap.offer(newStone);
+            }
+        }
+
+        return maxHeap.size() == 0 ? 0 : maxHeap.poll();
+        
+    }
+}
