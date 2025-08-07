e1feaea2c11111d3649459acb5d310040040d0ce
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-08-07 10:19:40 -0400

    Time: 2 ms (79.92%), Space: 46.1 MB (64.65%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int slow = 0;
+        int fast = 0;
+
+        while (fast < nums.length) {
+            if (nums[fast] != 0){
+                int temp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = temp;
+                slow += 1;
+            }
+            fast += 1;
+        }
+        
+    }
+}
