f62bcdcb83e330c2359a07fd06dce61d7cfe3875
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-09-04 07:01:25 -0400

    Time: 17 ms (20.38%), Space: 18 MB (10.98%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,23 +1,19 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
 
-        # we know that sqrt of x is in range of 0 to x/2+1
-
-        left, right = 0, 1 + (x//2)
-
-        while left<=right:
-
-            mid = left + (right-left)//2
-
-            square = mid*mid
-
-            if square==x:
-                return mid
-
-            if square<x:
+        while left < right:
+            mid = (right + left)//2
+            compute = mid * mid
+            print("mid", mid)
+            if compute <= x:
                 left = mid + 1
             else:
                 right = mid - 1
 
-        return right
+        return right if right * right <= x else right - 1
+
+
+
         
