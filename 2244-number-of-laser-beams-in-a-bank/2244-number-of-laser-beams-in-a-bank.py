b51904b7148925f9ac3f8653b46fee5d970846ae
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-10-26 23:52:09 -0400

    Time: 143 ms (13.26%), Space: 19.4 MB (77.78%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
@@ -1,12 +1,32 @@
 class Solution:
+
+    def getEdges(self, input: str) -> int:
+        count: int = 0
+        for e in input:
+            if e == '1':
+                count += 1
+        return count
+
     def numberOfBeams(self, bank: List[str]) -> int:
+
+        edges = 0
         
-        curr=bank[0].count("1")
-        beams=0
-        for row in bank[1:]:
-            devices=row.count("1")
-            if not devices:
-                continue
-            beams+=devices*curr
-            curr=devices
-        return beams
+        for i in range(len(bank)):
+            curRow = self.getEdges(bank[i])
+            if curRow == 0: continue
+            jRow = 0
+            j = i+1
+            while j < len(bank):
+                jRow = self.getEdges(bank[j])
+                j += 1
+                if jRow > 0:
+                    break
+
+            edges += curRow * jRow
+            
+            
+
+        return edges
+            
+
+        
