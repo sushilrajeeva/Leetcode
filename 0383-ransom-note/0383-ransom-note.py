a6cf44fd0489af65e41e350764abba2aa7864574
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-11-03 03:48:00 -0500

    Time: 13 ms (81.25%), Space: 18 MB (34.35%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,8 +1,19 @@
 class Solution:
+
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        if len(ransomNote)>len(magazine):
-            return False
-        for c in set(ransomNote):
-            if magazine.count(c)<ransomNote.count(c):
+        n = len(ransomNote)
+        m = len(magazine)
+
+        if n > m: return False
+
+        charMap = [0] * 26
+
+        for ch in magazine:
+            charMap[ord(ch) - 97] += 1
+        
+        for ch in ransomNote:
+            charMap[ord(ch) - 97] -= 1
+            if charMap[ord(ch) - 97] < 0:
                 return False
+
         return True
