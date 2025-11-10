25a4eae3cfd6bca4ffed793eb27c38a6e78dc002
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-11-09 22:27:57 -0500

    Time: 15 ms (41.04%), Space: 18 MB (49.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,21 @@
 class Solution:
+
+    def getIndex(self, ch: str) -> int:
+        return ord(ch) - 97
+
     def isAnagram(self, s: str, t: str) -> bool:
+        m, n = len(s), len(t)
+        if m != n: return False
 
-        n: int = len(s)
-        m: int = len(t)
+        s_arr = [0] * 26
+        t_arr = [0] * 26
 
-        if n != m: return False
-
-        alphabetBuffer: List[int] = [0] * 26
-
-        for i in range(n):
-            alphabetBuffer[ord(s[i])-97] += 1
-            alphabetBuffer[ord(t[i])-97] -= 1
+        for i in range(m):
+            s_arr[self.getIndex(s[i])] += 1
+            t_arr[self.getIndex(t[i])] += 1
 
         for i in range(26):
-            if alphabetBuffer[i] != 0: return False
-        
+            if s_arr[i] != t_arr[i]: return False
+
         return True
         
