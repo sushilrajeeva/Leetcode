73ce297ae76aa5770fa0a3c23bea0c24016e0331
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-11-11 17:03:17 -0500

    Time: 63 ms (55.66%), Space: 17.7 MB (91.66%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,4 +1,13 @@
 class Solution:
+    def isPalindrome(self, s: str, l: int, r: int) -> bool:
+        while l < r:
+            if s[l] == s[r]:
+                l += 1
+                r -= 1
+                continue
+            else: return False
+        return True
+
     def validPalindrome(self, s: str) -> bool:
         count = 0
         left = 0
@@ -6,14 +15,9 @@ class Solution:
         right = n - 1
 
         while left < right:
-            if s[left] == s[right]:
-                left += 1
-                right -= 1
-                continue
-            count += 1
-            if count > 1: return False
-            if s[left] == s[right - 1]:
-                right -= 1
-            else: left += 1
+            if s[left] != s[right]:
+                return self.isPalindrome(s, left, right-1) or self.isPalindrome(s, left+1, right)
+            left += 1
+            right -= 1
         return True
         
