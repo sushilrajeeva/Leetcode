acc91395e342910f687f04730faa5f3db4c2fc04
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-11-20 06:03:45 -0500

    Time: 77 ms (56.75%), Space: 18.6 MB (72.97%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -4,12 +4,26 @@ class MyCalendar:
         self.calendar = []
 
     def book(self, start, end):
-        for s, e in self.calendar:
-            if s < end and start < e:
-                return False
-        self.calendar.append((start, end))
-        return True
+        low = 0
+        high = len(self.calendar) - 1
 
+        if high == -1:
+            self.calendar.append((start, end))
+            return True
+        
+        while low <= high:
+            mid = low + (high - low) //2
+            mid_start, mid_end = self.calendar[mid]
+            if (max(start, mid_start) < min(end, mid_end)):
+                return False
+            
+            elif mid_start < start:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        self.calendar.insert(low, (start, end))
+        return True
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
