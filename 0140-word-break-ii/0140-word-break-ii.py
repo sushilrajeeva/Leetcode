9942226c8af54bd4d51e9379b8b6f955d77943f4
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-11-23 11:32:51 -0500

    Time: 0 ms (100%), Space: 17.7 MB (92.06%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        temp = []
+
+        def dfs(i):
+            if i == len(s):
+                res.append(" ".join(temp))
+            for w in wordDict:
+                n = len(w)
+                if (i + n) <= len(s) and s[i: i + n] == w:
+                    temp.append(s[i: i + n])
+                    dfs(i + n)
+                    temp.pop()
+
+        dfs(0)
+        return res
