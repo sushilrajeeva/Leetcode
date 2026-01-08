8a4c8569a096423e4913ca83b4082c67c056875b
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2026-01-08 05:17:04 -0500

    Time: 4818 ms (66.32%), Space: 19.9 MB (24.53%) - LeetHub

---
+++ 3743-maximize-cyclic-partition-score/3743-maximize-cyclic-partition-score.py
@@ -0,0 +1,20 @@
+fmax = lambda a, b: b if b > a else a
+
+class Solution:
+    # 3573. Best Time to Buy and Sell Stock V
+    def maximumProfit(self, prices: List[int], k: int) -> int:
+        f = [[-inf] * 3 for _ in range(k + 2)]
+        for j in range(1, k + 2):
+            f[j][0] = 0
+        for p in prices:
+            for j in range(k + 1, 0, -1):
+                f[j][0] = fmax(f[j][0], fmax(f[j][1] + p, f[j][2] - p))
+                f[j][1] = fmax(f[j][1], f[j - 1][0] - p)
+                f[j][2] = fmax(f[j][2], f[j - 1][0] + p)
+        return f[-1][0]
+
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        max_i = nums.index(max(nums))
+        ans1 = self.maximumProfit(nums[max_i:] + nums[:max_i], k)  # nums[max_i] is the first element.
+        ans2 = self.maximumProfit(nums[max_i + 1:] + nums[:max_i + 1], k)  # nums[max_i] is the last element.
+        return fmax(ans1, ans2)
