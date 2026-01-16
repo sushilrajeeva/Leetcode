f8f5b30aa14f50ca0315d5f31f47fefc74932ac7
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2026-01-16 05:09:44 -0500

    Time: 1944 ms (5.08%), Space: 29.8 MB (8.14%) - LeetHub

---
+++ 2514-count-anagrams/2514-count-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countAnagrams(self, s: str) -> int:
+
+        def get(word):
+            n = math.factorial(len(word))
+
+            for v in Counter(word).values():
+                n //= math.factorial(v)
+
+            return int(n) % (10**9 + 7)
+
+        total = 1  
+        for word in s.split():
+            total *= get(word)
+
+        return total % (10**9 + 7)
